e563bf0120b549835f9d676970e27fd1f45d012e
Emily Ruppel <emily.k.ruppel@gmail.com>
2022-03-29 22:46:02 -0400

    changes to add catnap scheduler

--- apps/blink/src/main.c
+++ apps/blink/src/main.c
@@ -32,7 +32,7 @@ void burn();
 
 STARTER_EVT(starter);
 
-DEC_EVT(blink,blink,4000); //TODO translation from seconds to ticks
+DEC_EVT(blink,blink,8000); //TODO translation from seconds to ticks
 DEC_EVT(blink2,blink2,7000); //TODO translation from seconds to ticks
 DEC_TSK(burn,burn);
 
@@ -50,43 +50,33 @@ uint16_t val = 0;
 uint8_t new_adc_val = 0;
 
 void blink() { //16000
+  LCFN_INTERRUPTS_DISABLE;
   PRINTF("Blinking!\r\n");
-  ADC_ENABLE;
-	// Set and fire Timer A1
-  //TA0CCTL0 &= ~CCIE;
-	TA1CCR0 = 8000;
-	TA1CTL = MC__UP | TASSEL__SMCLK;
-  TA1CCTL0 = CCIE;
-  while(1) {
-    BIT_FLIP(1,0);
-    if (new_adc_val) {
-      TA1CCTL0 &= ~CCIE; //dis
-      PRINTF("Got adc val: %u\r\n",val);
-      TA1CCTL0 |= CCIE; //en
-      new_adc_val = 0;
-    }
-  }
-  ADC_DISABLE;
+  LCFN_INTERRUPTS_ENABLE;
   EVENT_RETURN();
 }
 
 void blink2() { //4000
+  LCFN_INTERRUPTS_DISABLE;
   PRINTF("Still blinking!\r\n");
+  LCFN_INTERRUPTS_ENABLE;
   EVENT_RETURN();
 }
 
 
 void burn() {
-    for (int k = 0; k < 0xff; k++) {
+  while(1) {
+    for (int k = 1; k < 0xff; k++) {
       for( int j = 0; j < 0xff; j++) {
         for(int i = 0; i < 0xff; i++) {
           volatile uint16_t temp = i;
         }
       }
-      DISABLE_LFCN_TIMER;
+      LCFN_INTERRUPTS_DISABLE;
       PRINTF("\t\tTasking %u\r\n",k);
-      ENABLE_LFCN_TIMER;
+      LCFN_INTERRUPTS_ENABLE;
     }
+  }
   return;
 }
 
@@ -116,7 +106,8 @@ void __attribute__((interrupt(TIMER1_A0_VECTOR)))
 timerA1ISRHandler(void) {
 	  //TA1CCTL0 &= ~(CCIFG | CCIE); // Clear flag and stop int
     //BIT_FLIP(1,0);
-    BIT_FLIP(1,1);
+    BIT_FLIP(1,4);
+    BIT_FLIP(1,5);
     TA1R = 0;
     val = read_adc();
     new_adc_val = 1;
---
+++ tools/gen_comp.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+NUM_TAPS = 32
+DIVIDER = 4.22/14.22
+
+refs = [1.2,2.0,2.5]
+
+ref_regs = {1.2:1, 2.0:2, 2.5:3}
+
+def calc_tap(val,divider,num_taps):
+  diff = 1
+  best_ref = 0
+  best_tap = 0
+  for ref in refs:
+    tap_val =  np.floor(num_taps*val*divider/ref)
+    test = tap_val*ref/(num_taps*divider)
+    if (abs(val - test) < diff):
+      diff = abs(val - test)
+      best_ref = ref
+      best_tap = tap_val
+    #print(abs(val - test))
+  return [int(best_tap),best_ref]
+
+
+
+total = 0
+
+if __name__ == "__main__":
+  for val in np.arange(1.4,2.5,.02):
+    total += 1
+    [tap,ref] = calc_tap(val,DIVIDER,NUM_TAPS)
+    #CEREFL_2 | CEREF0_19 | CEREF1_20, // 10
+    val_str = "{:.2f}".format(val)
+    cerefl_str = "CEREFL_"+str(ref_regs[ref])
+    ceref0_str = "CEREF0_"+str(tap)
+    cerefl1_str = "CEREF1_"+str(tap+1)
+    print("/*",val_str,"*/",cerefl_str,"|",ceref0_str,"|",cerefl1_str,",")
+  for count,val in enumerate(np.arange(1.4,2.5,.02)):
+    dec_str = str(int(100*(val - np.floor(val))))
+    dec_str = dec_str.zfill(2)
+    volt_str = "V_"+str(int(np.floor(val)))+"_"+dec_str
+    print(volt_str,"=",count,",")
+  for val in np.arange(1.4,2.5,.02):
+    hndrds_str = str(int(100*val))
+    print(hndrds_str,",")
+  print("Total: ",total)
+
+
+
