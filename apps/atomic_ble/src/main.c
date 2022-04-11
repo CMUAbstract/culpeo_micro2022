f1cd5ff36c693721fceac7e24bb019c80a4f591f
Emily Ruppel <emily.k.ruppel@gmail.com>
2022-04-10 22:13:23 -0400

    just notching progress for app with interrupt triggered events

--- apps/atomic_ble/src/main.c
+++ apps/atomic_ble/src/main.c
@@ -46,17 +46,28 @@ void extra_sense();
 
 STARTER_EVT(starter);
 
-#define BLE_PERIOD 24000
+#define BLE_PERIOD 30000
 
 DEC_EVT(radio,radio,BLE_PERIOD,SPORADIC); //Deschedules itself
 DEC_EVT(encrypt,encrypt,BLE_PERIOD,SPORADIC);//Deschedules itself
-DEC_EVT(sense,sense,BLE_PERIOD,PERIODIC);
 DEC_TSK(extra_sense,extra_sense);
+#ifndef RUN_INT
+DEC_EVT(sense,sense,BLE_PERIOD,PERIODIC);
+#else
+DEC_EVT(sense,sense,BLE_PERIOD,SPORADIC);
+#endif
 
 uint16_t min_reading = 0xFFFF;
 
 void app_hw_init(void) {
   enable_photoresistor();
+#ifdef RUN_INT
+  // Init interrupt on P1.5
+  P1DIR &= ~BIT5; //input
+  P1REN |= BIT5; //pulldown
+  P1IES &= ~BIT5; //low to high
+  P1IE |= BIT5; //enable int
+#endif
 }
 
 void starter() {
@@ -71,6 +82,7 @@ void starter() {
 void sense() {
   LCFN_INTERRUPTS_DISABLE;
   // Init lsm6ds1
+  BIT_FLIP(1,0);
   fxl_clear(BIT_SENSE_SW);
   __delay_cycles(640000);
   fxl_set(BIT_SENSE_SW);
@@ -97,8 +109,13 @@ void sense() {
   }
   LCFN_INTERRUPTS_DISABLE;
   lsm_disable(); //off
+  fxl_clear(BIT_SENSE_SW);
+  __delay_cycles(640000);
   LCFN_INTERRUPTS_ENABLE;
-  add_event(&EVENT(encrypt));//TODO add back in
+  add_event(&EVENT(encrypt));
+#ifdef RUN_INT
+  dec_event(&EVENT(sense));
+#endif
   EVENT_RETURN();
 }
 
@@ -116,11 +133,8 @@ void encrypt() {
 }
 
 void radio() {
-  BIT_FLIP(1,1);
-  BIT_FLIP(1,1);
   PRINTF("Radio!\r\n");
   for(int j = 0; j < 8; j++) {
-    BIT_FLIP(1,0);
     for (unsigned i = 0; i < PKT_LEN; ++i) {
         radio_buff[i] =*(((uint8_t *)&PLAINTEXT) + (j*PKT_LEN)); }
     radio_send_one_off();
@@ -132,30 +146,37 @@ void radio() {
 
 void extra_sense() {
   while(1) { // Run perpetually
+    uint32_t avg_sum0;
+    uint32_t avg_sum1;
     uint16_t light, light1;
     for (int i = 0; i < 16; i++) {
-      LCFN_INTERRUPTS_DISABLE;
-      //PRINTF("\t\tStart task\r\n");
-      //enable_photoresistor();
-      light = read_photoresistor();
-      //Instead of delay?
-      //enable_photoresistor();
-      light1 = read_photoresistor();
-      LCFN_INTERRUPTS_ENABLE;
-      //TODO add an averaging step instead, write avg & stddev
-      for( int j = 0; j < 0xff; j++) {
-        for(int i = 0; i < 0xff; i++) {
-          volatile uint16_t temp = i;
-        }
+      avg_sum0 = 0;
+      avg_sum1 = 0;
+      for (int j = 0; j < 16; j++) {
+        LCFN_INTERRUPTS_DISABLE;
+        light = read_photoresistor();
+        __delay_cycles(8000);
+        light1 = read_photoresistor();
+        LCFN_INTERRUPTS_ENABLE;
+        avg_sum0 += light;
+        avg_sum1 += light1;
       }
-      LCFN_INTERRUPTS_DISABLE;
-      PRINTF("Tasking %u!\r\n",i);
-      __delay_cycles(1000);
-      LCFN_INTERRUPTS_ENABLE;
+      light = (uint16_t)(avg_sum0 >> 4);
+      light1 = (uint16_t)(avg_sum1 >> 4);
       PLAINTEXT[(i<<4) + 12] = light;
       PLAINTEXT[(i<<4) + 14] = light1;
     }
   }
 }
 
-
+#ifdef RUN_INT
+void __attribute__((interrupt(PORT1_VECTOR)))
+port1_handler(void) {
+  P1IE &= ~BIT5;
+  P1IFG = 0;
+  add_event(&EVENT(sense));
+  PRINTF("Adding event!\r\n");
+  P1IE |= BIT5;
+  INT_RETURN;
+}
+#endif
